1385a894f37b4cc4f60bb7cfa7726bf20084a1e4
Levin_Ihor <lytghzys@gmail.com>
2022-08-29 17:32:27 +0300

    Aside left component

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,110 +1,6 @@
 <template>
-  <main :class="{ scroll: onScroll }">
-    <VHeader @onScroll="onScroll = true" @offScroll="onScroll = false" />
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus magni
-    quibusdam provident iste, quae minus veniam at quisquam explicabo nisi,
-    perferendis amet consequuntur nihil excepturi harum fugit dolor velit sit
-    tempore maxime cupiditate, officia impedit maiores doloremque. Eum quo ab
-    quas animi, soluta magnam, temporibus hic expedita distinctio illo odio ex
-    quod alias quis, aliquam pariatur incidunt dicta qui odit possimus iure? Nam
-    ab iusto facilis distinctio eaque veniam sunt odit libero quibusdam?
-    Sapiente quia ad doloremque facilis modi nostrum esse excepturi expedita
-    dolorum nihil! Omnis adipisci molestiae dignissimos facere eius sit ipsum,
-    beatae similique non quasi? Vitae sunt veritatis, rem inventore quis
-    praesentium hic laboriosam eos voluptatem impedit ex neque voluptate dolorem
-    alias doloribus quibusdam explicabo, quod earum reprehenderit quia
-    asperiores. Inventore quia aliquid hic odio? Dolorum error recusandae sed
-    veritatis distinctio reprehenderit, deleniti voluptas consectetur ipsum quas
-    asperiores cupiditate ullam? Earum aperiam, natus officiis repellendus
-    cumque voluptatum amet dolor! Harum dolorum minus distinctio modi qui
-    impedit assumenda quaerat iste, amet ea iusto, totam quis at? Consectetur
-    inventore aliquid minus commodi. Sint delectus numquam excepturi cupiditate
-    reiciendis obcaecati tenetur asperiores fugiat quibusdam, alias aspernatur
-    unde earum molestiae ea vero corporis velit facilis placeat hic laudantium
-    quidem neque perferendis sed deserunt! Ipsam quod a dolor blanditiis nemo
-    doloribus ut nisi adipisci. Nostrum, tempora quibusdam excepturi quo ullam
-    omnis, consectetur unde aliquid repudiandae sunt quia! Dignissimos soluta
-    temporibus ullam culpa, debitis non aliquid libero ipsum alias inventore
-    voluptates est totam, suscipit voluptatum quo error consequatur voluptate
-    quod enim! Mollitia quo molestias quibusdam culpa dolor rem delectus impedit
-    saepe unde. Placeat voluptatum incidunt autem, odit doloribus molestias
-    iusto ducimus. Mollitia consectetur et necessitatibus deserunt vero repellat
-    perspiciatis illum! Atque asperiores sapiente cumque voluptate excepturi
-    inventore laboriosam ullam praesentium nulla. Esse, dolores itaque? Numquam
-    quasi iste officiis vitae nulla incidunt id voluptas voluptatum aspernatur
-    recusandae. Dolorum harum fugit quis assumenda magni aliquid molestias unde
-    laudantium ratione, id temporibus rerum est quidem et veniam error labore
-    illum nam minima ut pariatur expedita rem eveniet. Cupiditate fugit sint
-    voluptatum temporibus labore fugiat necessitatibus non perferendis ut nihil
-    aliquam cumque illo quisquam similique minima, reiciendis quia excepturi
-    dolor debitis enim. Fuga tempora temporibus veritatis, dolorem amet eum
-    quaerat voluptatibus sunt voluptate maxime dignissimos iusto optio rerum
-    praesentium in error cum enim eligendi ipsam iste sed aperiam eius rem?
-    Voluptates ut, culpa esse quibusdam eveniet maiores explicabo perspiciatis
-    sit quaerat! Blanditiis quia odit quisquam ea. Dignissimos officia, iure,
-    corporis soluta nesciunt molestiae hic velit voluptas dicta ut nisi
-    voluptate minima? Enim, recusandae maxime incidunt ipsam odio eaque non
-    iusto quisquam laboriosam ducimus ipsa numquam, similique debitis beatae
-    labore hic harum cumque ab tempora magni neque dolorum. Necessitatibus
-    commodi iste quod, assumenda architecto autem voluptas libero veniam nihil
-    nemo perspiciatis ipsa, modi officia molestias at fugiat, omnis accusamus
-    laboriosam. Delectus repellat blanditiis aliquam aspernatur quidem facilis,
-    molestiae quae aut omnis. Molestias quos at ab dicta similique vitae culpa,
-    minima commodi consequuntur, nisi veritatis quod cum sit doloremque a ipsum
-    tempore expedita voluptatum, nihil sed earum. Earum consectetur perspiciatis
-    eos quasi ut dignissimos suscipit, natus quibusdam reiciendis eveniet, ullam
-    ipsum, commodi rerum! Possimus ex quibusdam explicabo, aut ab nisi commodi
-    libero magni pariatur maxime doloremque ratione laboriosam voluptas at
-    facilis neque. Saepe perspiciatis dicta nihil nostrum? Impedit voluptate
-    incidunt consectetur eius, modi possimus maiores explicabo tenetur delectus
-    voluptatum pariatur officiis est facilis, omnis reprehenderit amet sapiente!
-    Ducimus laborum veniam recusandae harum quibusdam deleniti impedit eveniet
-    sequi accusantium eaque architecto perspiciatis numquam error quas tempora
-    earum corrupti in, eum ullam omnis consequatur necessitatibus ex aliquid.
-    Voluptas, neque ea. Facilis consectetur odio qui nulla, itaque ipsam eos.
-    Voluptate rerum odio maxime accusantium cupiditate provident alias, expedita
-    quidem optio voluptatibus quaerat aspernatur excepturi atque quae quod at in
-    magni enim tempore itaque ea repudiandae consequuntur asperiores? Est omnis
-    modi placeat corrupti delectus! Sunt debitis pariatur quidem aliquam cum
-    voluptates velit corrupti, iure fugit assumenda exercitationem laudantium
-    tempora qui maxime consectetur ea quia perferendis labore nulla voluptatum
-    in dolor totam? Deserunt quasi aliquid dolorem iusto commodi dignissimos
-    nobis! Amet animi, nisi perferendis nostrum ratione laborum odit commodi.
-    Repudiandae, fugiat autem reiciendis cumque illum, nam tenetur pariatur odit
-    velit saepe iste maxime. Dolorum, temporibus ad distinctio reiciendis porro
-    est rerum adipisci unde odio ex quibusdam, quod aspernatur sequi quo
-    asperiores amet ullam possimus dignissimos labore minima fugit earum.
-    Adipisci ipsam quibusdam facilis perferendis ut odit asperiores dolores
-    aperiam at non provident placeat, ex id ullam natus cupiditate, obcaecati
-    rerum, voluptatibus exercitationem voluptatem necessitatibus nemo maiores
-    nesciunt enim. Vitae officia consequatur inventore hic corporis quisquam
-    recusandae repudiandae labore tempora illum enim omnis reprehenderit dicta
-    eos, commodi perferendis explicabo quos dolorem sequi maxime placeat tenetur
-    ut laborum repellendus. Dolor a minus voluptate rerum libero, neque, eaque
-    veritatis cupiditate asperiores eius ipsa est laborum perspiciatis
-    consequatur officiis eum ipsum accusantium deleniti voluptates commodi
-    doloribus ab. Repellat omnis odio totam excepturi repellendus sed maxime
-    vel, ab et, consequatur blanditiis eos est placeat officiis animi rerum
-    esse. Perspiciatis blanditiis reprehenderit sapiente ex quam dolore ab
-    placeat explicabo quasi saepe! Blanditiis iste consectetur reprehenderit
-    sint pariatur? Itaque quaerat, molestiae omnis esse nostrum rerum porro,
-    beatae quam excepturi ratione ipsum nesciunt reiciendis? Libero, unde
-    adipisci repellat aliquid accusantium incidunt itaque obcaecati omnis
-    molestias aperiam amet corporis. Aliquam, aspernatur laboriosam enim,
-    molestias accusamus ab sequi quis omnis sapiente autem cumque. Officiis
-    sequi pariatur quo optio illum voluptatem iure numquam deleniti placeat
-    minima explicabo, veniam atque dignissimos rerum ipsam laborum? Porro
-    deleniti praesentium officiis ex dolores magni quam quisquam nisi, velit
-    iusto rem explicabo nemo beatae rerum facere culpa ipsam odit doloribus
-    obcaecati quo! Quia magnam vel nam praesentium perferendis facere ratione
-    officia recusandae laborum, perspiciatis eligendi numquam cupiditate
-    provident harum voluptate deleniti dolor vero dignissimos deserunt facilis,
-    blanditiis repellat! Nam animi aperiam assumenda iste. Nam, quae porro, esse
-    ex atque quod iste facere aliquam adipisci soluta minima quibusdam molestiae
-    totam, rerum recusandae exercitationem laudantium placeat expedita deserunt
-    laboriosam? Perspiciatis voluptate ad similique doloremque nisi officia nam
-    sapiente quos saepe voluptatibus doloribus culpa dolorum, omnis unde amet
-    blanditiis id et ipsam mollitia ducimus!
+  <main>
+    <VHeader />
     <VWelcome />
     <VAbout />
     <VExperience />
@@ -125,10 +21,6 @@ import VContact from "@/components/v-contact.vue";
 import VFooter from "@/components/v-footer.vue";
 import VAsideLeft from "@/components/v-aside-left.vue";
 import VAsideRight from "@/components/v-aside-right.vue";
-
-import { ref } from "vue";
-
-const onScroll = ref(false);
 </script>
 
 <style scoped>
@@ -138,7 +30,7 @@ main {
 
   margin: 0 auto;
   color: aliceblue;
-  padding: 1px;
+  padding: 0px 100px;
   min-height: 100%;
   overflow: hidden;
 }
